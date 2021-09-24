45438c3806f45ac9ca00eba65fe7cf7921829e67
junior abad <jabad@cajapiura.pe>
2021-09-24 11:22:14 -0500

    Autentiticacion con google

--- app/src/androidTest/java/pe/japdesar/serviagro/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/pe/japdesar/serviagro/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("pe.juabsa.serviagro", appContext.packageName)
+        assertEquals("pe.japdesar.serviagro", appContext.packageName)
     }
 }
